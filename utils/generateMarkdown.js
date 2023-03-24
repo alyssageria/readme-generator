38e4c6c10f7e44180dae66e28241e58e23b1f8e9
Alyssa Geria <alyssageria@gmail.com>
2023-03-24 12:09:56 -0700

    added renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,21 @@ const index = require('../index.js');
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    } else if (license === "APACHE2.0") {
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    } else if (license === "Boost1.0") {
+        return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+    } else if (license === "BSD2") {
+        return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+    } else if (license === "BSD3") {
+        return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+    } else if (license === "none") {
+        return "";
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
